db60a27a25c5bfdb91e2ccf0feeb36a367d83539
coderATP <atandatosinpete@gmail.com>
2025-05-31 07:05:14 +0100

    update PreloadScene.js

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -269,7 +269,7 @@ export class PreloadScene extends BaseScene{
             })
         })
         eventEmitter.once("PRELOAD_TO_MENU", ()=>{
-           // this.toggleFullscreen()
+            this.toggleFullscreen()
             this.scene.start("MenuScene");
         })
         //load audio
